f85462f38f75d6a5c1950831eb38adcaa08dbb8d
pank10190 <114277166+pank10190@users.noreply.github.com>
2026-03-12 11:10:10 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
+import LandingPage from "./pages/LandingPage.jsx";
 import Login from "./pages/admin/Login";
 import Dashboard from "./pages/admin/Dashboard";
 import Users from "./pages/admin/Users";
